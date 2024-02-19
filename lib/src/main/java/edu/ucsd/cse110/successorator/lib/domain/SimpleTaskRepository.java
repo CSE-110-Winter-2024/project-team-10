1750e508114f995b0b3456c16ba3a6f06a2f2e10
BridgetteZagrebin <103292060+BridgetteZagrebin@users.noreply.github.com>
2024-02-18 23:01:21 -0800

    Button for date display is pulled into this branch now, only need tests now

---
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/SimpleTaskRepository.java
@@ -0,0 +1,40 @@
+//package edu.ucsd.cse110.successorator.lib.domain;
+//
+//import java.util.List;
+//import java.util.Objects;
+//
+//import edu.ucsd.cse110.successorator.lib.data.InMemoryDataSource;
+//import edu.ucsd.cse110.successorator.lib.util.Subject;
+//
+//public class SimpleTaskRepository implements TaskRepository {
+//    private final InMemoryDataSource dataSource;
+//
+//    public SimpleTaskRepository(InMemoryDataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+//
+//    @Override
+//    public Subject<Task> find(String description) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Subject<List<Task>> findAll() {
+//        return dataSource.getTaskListSubject();
+//    }
+//
+//    @Override
+//    public void save(Task task) {
+//        dataSource.addTask(task);
+//    }
+//
+//    @Override
+//    public int size() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public void completed(int id) {
+//        dataSource.completed(id, dataSource);
+//    }
+//}
